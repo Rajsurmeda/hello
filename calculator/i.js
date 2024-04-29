cccc0ca7b2cbcdede7479b4362c5ade1bef86b8e
Rajesh surmeda <160104433+Rajsurmeda@users.noreply.github.com>
2024-04-29 16:06:25 +0500

    Add files via upload
    my project

---
+++ calculator/i.js
@@ -0,0 +1,19 @@
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function calculate() {
+    let result = document.getElementById('result').value;
+    if (result) {
+        try {
+            document.getElementById('result').value = eval(result);
+        } catch (error) {
+            document.getElementById('result').value = 'Error';
+            
+        }
+    }
+}
